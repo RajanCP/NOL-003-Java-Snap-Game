e58fe3069ec26458e7d2c06fd09d39346bc6d5f3
RajanCP <rjnptl2000@gmail.com>
2025-07-29 18:47:28 +0100

    S4: Added emojis and spacing for better game UI through console

--- src/main/java/Snap.java
+++ src/main/java/Snap.java
@@ -23,23 +23,35 @@ public class Snap extends CardGame {
 
         while (!getDeck().isEmpty()) {
             Player currentPlayer = isPlayerOneTurn ? player1 : player2;
+            Player opponentPlayer = isPlayerOneTurn ? player2 : player1;
+
             System.out.print(currentPlayer.getName() + " Press Enter to deal a card...");
             scanner.nextLine(); // waits for Enter
 
             Card currentCard = dealCard();
+
+            System.out.println("==================================");
             System.out.println(currentPlayer.getName() + " drew: " + currentCard);
+            System.out.println("==================================");
 
             if (previousCard != null && currentCard.rank() == previousCard.rank()) {
-                System.out.println("SNAP Opportunity! Type 'snap' within 2 seconds to WIN!");
+                System.out.println(" SNAP Opportunity! ");
+                System.out.println("Type 'snap' within 2 seconds to win!");
+                System.out.print(">> ");
 
                 long startTime = System.currentTimeMillis();
                 String input = scanner.nextLine();
                 if ((System.currentTimeMillis() - startTime) <= 2000 && input.equalsIgnoreCase("snap")) {
-                    System.out.println("");
+                    System.out.println("\uD83C\uDF89 " + currentPlayer.getName() + " Wins!");
+                    return;
                 } else if (System.currentTimeMillis() - startTime > 2000) {
-                    System.out.println("Too slow!");
+                    System.out.println(" Too slow! " + currentPlayer.getName() + " loses.");
+                    System.out.println("\uD83C\uDFC6 " + opponentPlayer.getName() + " wins by default!");
+                    return;
                 } else {
-                    System.out.println("Wrong input.");
+                    System.out.println("Wrong input. " + currentPlayer.getName() + " loses.");
+                    System.out.println("\uD83C\uDFC6 "  + opponentPlayer.getName() + " wins by default!");
+                    return;
                 }
             }
 
